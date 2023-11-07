dc5cfb82337594bcabea5ba34cde97f5fe96fee5
Marcus BONED <marcusbonedth@gmail.com>
2023-11-07 09:24:56 +0100

    feat: creation de la structure de la page error404

---
+++ reglement.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="reglement.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <section id="fond">
+        <h1 id="titre">Règlement de la mmi lan</h1>
+
+    <div>
+        <div>
+            <h2>Généralités</h2>
+            <img src="FlecheRose.svg" alt="">
+        </div>
+        <div></div>
+    </div>
+    <div>
+        <div>
+            
+        </div>
+        <div></div>
+    </div>
+    <div>
+        <div></div>
+        <div></div>
+    </div>
+    <div>
+        <div></div>
+        <div></div>
+    </div>
+    <div>
+        <div></div>
+        <div></div>
+    </div>
+    <div>
+        <div></div>
+        <div></div>
+    </div>
+    <div>
+        <div></div>
+        <div></div>
+    </div>
+    <div>
+        <div></div>
+        <div></div>
+    </div>
+    </section>
+    
+</body>
+</html>
