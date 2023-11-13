bd64053e608dae1fe329d116ba8b11e4b7eaad24
Alexandre <alexandre.testa.at@gmail.com>
2023-11-13 12:23:52 +0100

    régalge du css avec back et popup

---
+++ MLB-8_functions.php
@@ -0,0 +1,95 @@
+<?php
+function getTeamName($teamId, $db)
+{
+    $teamName = $db->prepare("SELECT TeamName FROM team WHERE TeamId=?");
+    $teamName->execute([$teamId]);
+    $nomequipe = $teamName->fetch()['TeamName'];
+    return $nomequipe;
+}
+
+function getTeamLogo($teamId, $db)
+{
+    $equipe = $db->prepare("SELECT TeamLogo FROM team WHERE TeamId=?");
+    $equipe->execute([$teamId]);
+    $logoequipe = $equipe->fetch()['TeamLogo'];
+    return $logoequipe;
+}
+
+function getTeamDesc($teamId, $db)
+{
+    $equipe = $db->prepare("SELECT TeamDesc FROM team WHERE TeamId=?");
+    $equipe->execute([$teamId]);
+    $descequipe = $equipe->fetch()['TeamDesc'];
+    return $descequipe;
+}
+
+function getTeamMembers($teamId, $db)
+{
+    $membres = $db->prepare("SELECT PlayerId FROM belongteam WHERE TeamId=? AND BelongStatus=?");
+    $membres->execute([$teamId, "validé"]);
+    $nbMembres = 0;
+    while ($donnees = $membres->fetch()) {
+        echo '<div class="membre">';
+        $membre = $db->prepare("SELECT PlayerLastname, PlayerFirstname, PlayerPseudo, PlayerPicture, PlayerFavGame FROM player WHERE PlayerId=?");
+        $membre->execute([$donnees['PlayerId']]);
+        $membre = $membre->fetch();
+        $nommembre = $membre[0];
+        $prénommembre = $membre[1];
+        $pseudomembre = $membre[2];
+        $imagemembre = $membre[3];
+        $jeufavmembre = $membre[4];
+        echo '<img src="./assets/img/' . $imagemembre . '"/>';
+        echo '<h2>' . $pseudomembre . '</h2>';
+        echo '<p class="jeu">' . $jeufavmembre . '</p>';
+        echo '<p class="texte_presentation">' . $prénommembre . " " . $nommembre . " alias " . $pseudomembre . '</p>';
+        echo '</div>';
+        $nbMembres++;
+    }
+
+    //--- Partie où on affiche des cases vides en cas d'équipe incomplète ---
+    while ($nbMembres < 3) {
+        echo '<div class="equipe_incomplete">';
+        echo '<h2>Cette équipe est incomplète</h2>';
+        $req = $db->prepare("SELECT PlayerId FROM belongteam");
+        $req->execute();
+        $equipierId = $req->fetchAll();
+        $dansequipe = false;
+        foreach ($equipierId as $equipier) {
+            if (in_array($_SESSION['playerId'], $equipier)) {
+                $dansequipe = true;
+                break;
+            }
+        }
+        if (!$dansequipe) {
+            $message = "Rejoindre l'équipe";
+            echo '<a class="rejondre" href="details_equipes.php?teamId=' . $teamId . '&rejoindreEquipe=' . true . '">' . $message . '</a>';
+        }
+        echo '</div>';
+        $nbMembres++;
+    }
+}
+
+function showReturnButtons($teamId, $db)
+{
+    $nbEquipe =  $db->prepare("SELECT MAX(TeamId) FROM team");
+    $nbEquipe->execute();
+    $nbEquipe = $nbEquipe->fetch()[0];
+    if ($teamId > 1) {
+        echo '<a href="details_equipes.php?teamId=' . ($teamId - 1) . '"><img src="./assets/img/fleche_gauche.svg" alt="flèche gauche"></a>';
+    } else {
+        echo '<a href="details_equipes.php?teamId=' . $nbEquipe . '"><img src="./assets/img/fleche_gauche.svg" alt="flèche gauche"></a>';
+    }
+    echo ' <a class="retour" href="[fichier_test]equipes.php">Retour au listing des équipes</a> '; //A MODIFIER
+    if ($teamId < $nbEquipe) {
+        echo '<a href="details_equipes.php?teamId=' . ($teamId + 1) . '"><img src="./assets/img/fleche_droite.svg" alt="flèche gauche"></a>';
+    } else {
+        echo '<a href="details_equipes.php?teamId=1"><img src="./assets/img/fleche_droite.svg" alt="flèche gauche"></a>';
+    }
+}
+
+function showAlertForm()
+{
+    if (isset($_GET['rejoindreEquipe'])) {
+        echo '<script>blocAlert = document.getElementById("alertRejoindreEquipe"); blocAlert.style.display = "block";</script>';
+    }
+}
---
+++ details_equipes.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./assets/style/details_equipes.css">
+  <title>Details équipe
+    <?php
+    session_start();
+    require_once("MLB-8_functions.php"); //A MODIFIER
+    require_once("[fichier_test]connexionbdd.php"); //A MODIFIER
+    $teamId = $_GET['teamId'];
+    echo getTeamName($teamId, $db);
+    ?>
+  </title>
+
+  <!-- <script src="./assets/js/jquery.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      $(".rejoindre").click(function() {
+        $("body").css("opacity", "0.50");
+      });
+    });
+  </script> -->
+</head>
+
+<body>
+  <header></header>
+
+  <main>
+    <div id="equipe">
+      <h1><?php echo getTeamName($teamId, $db); ?></h1>
+      <div class="photo_equipe">
+        <img class="illu_1" src="./assets/img/icon1.svg" alt="Illustration">
+        <img src="./assets/img/<?php echo getTeamLogo($teamId, $db); ?>" alt="Logo equipe" /> <!-- A MODIFIER -->
+        <img class="illu_2" src="./assets/img/icon2.svg" alt="Illustration">
+      </div>
+      <p><?php echo getTeamDesc($teamId, $db); ?></p>
+    </div>
+
+    <div id="membres">
+      <?php echo getTeamMembers($teamId, $db); ?>
+    </div>
+
+    <!--A COMPLETER UNE FOIS LE SHOOTING FAIT ! -->
+    <div id="photo" hidden></div>
+
+    <div id="retour">
+      <?php echo showReturnButtons($teamId, $db); ?>
+    </div>
+  </main>
+  <div id="alertRejoindreEquipe" hidden>
+    <h2>Vous allez rejoindre l'équipe <?php echo getTeamName($teamId, $db); ?></h2>
+    <form action="rejoindreEquipe.php" method="post">
+      <textarea name="playerDesc" id="" cols="30" rows="10" placeholder="Ecrire une description de vous"></textarea>
+      <div>
+        <input type="button" onclick='window.location.href="details_equipes.php?teamId=<?php echo $teamId ?>"' value="J'ai changé d'avis">
+        <input type="submit" value="Rejoindre">
+      </div>
+
+    </form>
+  </div>
+  <?php showAlertForm(); ?>
+  <footer></footer>
+</body>
+
+</html>
