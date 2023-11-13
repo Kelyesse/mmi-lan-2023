978d4367e6dec8c6aeb85a7b31ae5ee6b3a10ab2
TESTA Alexandre <alexandre.testa.at@gmail.com>
2023-11-13 15:24:35 +0100

    correction bug affichage

--- rejoindreEquipe.php
+++ rejoindreEquipe.php
@@ -10,41 +10,40 @@ $req = $db->prepare("SELECT PlayerLastName, PlayerFirstName, PlayerEmail FROM pl
 $req->execute([$createurId]);
 $nomcreateur = $req->fetch()[1] . " " . $req->fetch()[0];
 $createurmail = $req->fetch()[2];
-// use PHPMailer\PHPMailer\PHPMailer;
-// use PHPMailer\PHPMailer\SMTP;
-// use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-// require('assets/libs/PHPMailer-master/src/PHPMailer.php');
-// require('assets/libs/PHPMailer-master/src/SMTP.php');
-// require('assets/libs/PHPMailer-master/src/Exception.php');
+require('assets/libs/PHPMailer-master/src/PHPMailer.php');
+require('assets/libs/PHPMailer-master/src/SMTP.php');
+require('assets/libs/PHPMailer-master/src/Exception.php');
 
-// $mail = new PHPMailer(true);
+$mail = new PHPMailer(true);
 
-// try {
-//     //A MODIFIER UNE FOIS HEBERGE
-//     $mail->isSMTP();
-//     $mail->Host       = 'localhost';
-//     $mail->SMTPAuth   = true;
-//     $mail->Username   = 'root';
-//     $mail->Password   = '';
-//     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-//     $mail->Port       = 80;
+try {
+    //A MODIFIER UNE FOIS HEBERGE
+    $mail->isSMTP();
+    $mail->Host = 'localhost';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'root';
+    $mail->Password = '';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port = 80;
 
-//     $mail->Encoding = 'base64';
-//     $mail->CharSet = 'UTF-8';
+    $mail->Encoding = 'base64';
+    $mail->CharSet = 'UTF-8';
 
-//     $mail->setFrom('email@test.fr', 'MMI LAN 2023');
-//     $mail->addAddress($createurmail);
-//     $mail->addReplyTo('email@test.fr', 'MMI LAN 2023');
-//     $mail->addCC('mmi.lan.dev2023@gmail.com');
+    $mail->setFrom('email@test.fr', 'MMI LAN 2023');
+    $mail->addAddress($createurmail);
+    $mail->addReplyTo('email@test.fr', 'MMI LAN 2023');
+    $mail->addCC('mmi.lan.dev2023@gmail.com');
 
-//     $mail->isHTML(true);
-//     $mail->Subject = "Demande d'adhésion à votre équipe de la MMI LAN";
-//     $mail->Body    = "<p>Bonjour ".$nomcreateur.", un participant à la MMI LAN souhaite rejoindre votre équipe</p><p>".$_POST['playerDesc']."</p><p>Pour accepter ou refuser sa demande, veuillez vous rendre sur <a href=''>votre page Mon Compte du site de la MMI LAN</a> </p>";
-//     $mail->AltBody = '';
-//     $mail->send();
-// } 
-// catch (Exception $e) {
-//     echo $email_not_sent . "<br>Merci de transmettre ces informations à l'administrateur : {$mail->ErrorInfo}";
-// }
+    $mail->isHTML(true);
+    $mail->Subject = "Demande d'adhésion à votre équipe de la MMI LAN";
+    $mail->Body = "<p>Bonjour " . $nomcreateur . ", un participant à la MMI LAN souhaite rejoindre votre équipe</p><p>" . $_POST['playerDesc'] . "</p><p>Pour accepter ou refuser sa demande, veuillez vous rendre sur <a href=''>votre page Mon Compte du site de la MMI LAN</a> </p>";
+    $mail->AltBody = '';
+    $mail->send();
+} catch (Exception $e) {
+    echo $email_not_sent . "<br>Merci de transmettre ces informations à l'administrateur : {$mail->ErrorInfo}";
+}
 header("Location: details_equipes.php?teamId=" . $_GET['teamId']);
