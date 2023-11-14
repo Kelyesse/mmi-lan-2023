df357e389f44c32b06979732ac77e023dbc42935
Marcus BONED <marcusbonedth@gmail.com>
2023-11-14 09:56:15 +0100

    fix: change section in main

--- error.php
+++ error.php
@@ -5,13 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Error404</title>
     <link rel="stylesheet" href="./assets/style/error.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
     
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-    <section id="background">
+    <main>
         <!--texte en fond-->
         <p id="desole">désolé</p>
         <!--premier div avec le texte et bouton-->
@@ -24,6 +21,6 @@
             <!--bouton-->
             <a href="index.php" id="backAcc">Retour à l'accueil</a>
         </div>
-    </section>
+    </main>
 </body>
 </html>
