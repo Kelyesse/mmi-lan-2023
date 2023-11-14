cb72e583f42511cb8018ba76c802f440772127be
Marcus BONED <marcusbonedth@gmail.com>
2023-11-14 09:53:46 +0100

    fix: changes requested

--- Mentionslegales.php
+++ Mentionslegales.php
@@ -3,15 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Mentions légales</title>
     <link rel="stylesheet" href="./assets/style/Mentionslegales.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-    <section id="background">
+    <main>
         <h1 id="title">Mentions légales</h1> 
         <hr class="line">
         <div class="part">
@@ -41,5 +37,5 @@
                 <p>Les documents diffusés en version électronique sur ce site ont fait l’objet de nombreuses relectures de notre part mais peuvent encore contenir des erreurs.<br><br>Si vous en constatez, n’hésitez pas à nous le faire savoir en nous contactant : mmilancom.2023@gmail.com.<br><br>Nous procéderons immédiatement aux rectifications correspondantes.</p>
             </div>
         </div>
-    </section>
+    </main>
 </body>
