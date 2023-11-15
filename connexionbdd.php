2291b58e6fbcabff0a5bc75bc7ed99df4824ce05
Matthieu Biville <98494589+FaFou01@users.noreply.github.com>
2023-11-15 23:04:29 +0100

    Update connexionbdd.php

--- connexionbdd.php
+++ connexionbdd.php
@@ -2,14 +2,14 @@
     function Connexion(){
         $hostname = 'localhost';  
         $username = 'root'; 
-        $password = 'root';
-        $db = 'mmi-lan';
+        $password = '';
+        $db = 'mmi_lan';
         // Data Source Name
-        $dsn = "mysql:host=$hostname;dbname=$db";
+        $dsn = "mysql:host=$hostname;dbname=$db;charset=utf8mb4";
         try {
-            $db = new PDO($dsn, $username, $password);
-            $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-            return $db;
+            $bdd = new PDO($dsn, $username, $password);
+            $bdd->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            return $bdd;
         }
         catch(PDOException $e){
             echo "Erreur de connection ! </br>";
