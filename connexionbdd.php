6da22aec83f501cd90194675fd197ca2a4f02883
Matthieu Biville <98494589+FaFou01@users.noreply.github.com>
2023-11-15 23:34:34 +0100

    Add files via upload

--- connexionbdd.php
+++ connexionbdd.php
@@ -1,21 +1,20 @@
 <?php 
-function Connexion(){
-    $hostname = 'localhost';  
-    $username = 'root'; 
-    $password = 'root';
-    $db = 'mmi-lan';
-    $dsn = "mysql:host=$hostname;dbname=$db;charset=utf8mb4";
-
-    try {
-        $db = new PDO($dsn, $username, $password);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        return $db;
+    function Connexion(){
+        $hostname = 'localhost';  
+        $username = 'root'; 
+        $password = '';
+        $db = 'mmi_lan';
+        // Data Source Name
+        $dsn = "mysql:host=$hostname;dbname=$db;charset=utf8mb4";
+        try {
+            $bdd = new PDO($dsn, $username, $password);
+            $bdd->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            return $bdd;
+        }
+        catch(PDOException $e){
+            echo "Erreur de connection ! </br>";
+            echo $e->getMessage();
+        }
     }
-    catch(PDOException $e){
-        echo "Erreur de connexion : </br>";
-        echo $e->getMessage();
-    }
-}
-$db = Connexion();
+    $db = connexion();
 ?>
