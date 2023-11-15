9591188adfd99a3a74c264bea44121ae5ad15391
Helios Martin <heliosmartin123@gmail.com>
2023-11-15 21:26:47 +0100

    fix href to homepage

--- error.php
+++ error.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Error404</title>
     <link rel="stylesheet" href="./assets/style/error.css">
-    
+
 </head>
+
 <body>
     <main>
         <!--texte en fond-->
@@ -19,8 +21,9 @@
                 <p id="exist">La page que vous cherchez n'existe pas</p>
             </div>
             <!--bouton-->
-            <a href="index.php" id="backAcc">Retour à l'accueil</a>
+            <a href="./" id="backAcc">Retour à l'accueil</a>
         </div>
     </main>
 </body>
+
 </html>
