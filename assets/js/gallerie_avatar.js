d12171a50313bb63eb4b2538667a52ba15fe6f12
weavyx <you@example.com>
2023-11-16 16:36:32 +0100

    fix img path in db

--- assets/js/gallerie_avatar.js
+++ assets/js/gallerie_avatar.js
@@ -1,32 +1,31 @@
-$(document).ready(function() {
-    let prem_img = $('.prem');
-    let sec_img = $('.sec');
-    let avatarOptions = $('.avatar-option');
-    let avatarInput = $('#avatar');
+$(document).ready(function () {
+  let prem_img = $(".prem");
+  let sec_img = $(".sec");
+  let avatarOptions = $(".avatar-option");
+  let avatarInput = $("#avatar");
 
+  sec_img.hide();
+
+  $("#next").click(function () {
+    prem_img.hide();
+    sec_img.show();
+  });
+
+  $("#pre").click(function () {
     sec_img.hide();
+    prem_img.show();
+  });
 
+  avatarOptions.on("click", function () {
+    avatarOptions.removeClass("active");
+    $(this).addClass("active");
 
-    $('#next').click(function(){
-        prem_img.hide();
-        sec_img.show();
-    });
+    var selectedAvatarSrc = $(this).find("img").attr("src");
 
-    $('#pre').click(function(){
-        sec_img.hide();
-        prem_img.show();
-    });
+    var fileName = selectedAvatarSrc.split("/").pop();
 
+    avatarInput.val(fileName);
 
-        avatarOptions.on('click', function() {
-            avatarOptions.removeClass('active');
-            $(this).addClass('active');
-
-            var selectedAvatarSrc = $(this).find('img').attr('src');
-            avatarInput.val(selectedAvatarSrc);
-
-            console.log(avatarInput.val());
-        });
-
-
+    console.log(avatarInput.val());
+  });
 });
