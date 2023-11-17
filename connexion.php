016138d7988080ee41d461764b55208b70453df6
Helios Martin <heliosmartin123@gmail.com>
2023-11-17 21:41:22 +0100

    redirection changed

--- connexion.php
+++ connexion.php
@@ -5,7 +5,7 @@ include("navbar.php");
 // Vérifie si $_SESSION['PlayerId'] existe
 if (isset($_SESSION['PlayerId'])) {
     // Si la session existe, redirige vers une autre page
-    header('Location: index.php');
+    header('Location: mon_compte.php');
     exit();
 }
 
@@ -41,8 +41,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['email']) && isset($_PO
                 // Créez un cookie pour stocker les informations de connexion
                 setcookie('remember_user', $PlayerId, time() + 60 * 60 * 24 * 30); // Valide pendant 30 jours
             }
-            // Partie à modifier : Rediriger vers la page d'accueil ou une autre page après la connexion réussie
-            header("Location: index.php");
+            header("Location: mon_compte.php");
             exit();
         } else {
             // L'utilisateur n'existe pas
@@ -70,20 +69,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['recover'])) {
     <title>Accueil | Connexion</title>
     <link rel="stylesheet" href="./assets/style/connexion.css" />
     <script>
-        document.addEventListener('DOMContentLoaded', function() {
+        document.addEventListener('DOMContentLoaded', function () {
             // Récupérer les éléments nécessaires
             var forgotPasswordLink = document.getElementById('forgot-password');
             var popup = document.getElementById('popup');
             var closePopupButton = document.getElementById('close-popup');
 
             // Ajouter un écouteur d'événement pour le clic sur "Mot de passe oublié ?"
-            forgotPasswordLink.addEventListener('click', function(e) {
+            forgotPasswordLink.addEventListener('click', function (e) {
                 e.preventDefault();
                 popup.style.display = 'block';
             });
 
             // Ajouter un écouteur d'événement pour le bouton de fermeture du popup
-            closePopupButton.addEventListener('click', function() {
+            closePopupButton.addEventListener('click', function () {
                 popup.style.display = 'none';
             });
         });
@@ -117,21 +116,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['recover'])) {
                     <div class="inputs">
                         <div class="flex-logo-input space1">
                             <img class="logo-input" src="./assets/img/mail.svg">
-                            <input class="style-input" type="email" name="email" placeholder="Entrer votre adresse mail" required />
+                            <input class="style-input" type="email" name="email" placeholder="Entrer votre adresse mail"
+                                required />
                         </div>
                         <div class="space2 flex-logo-input style-input">
                             <img class="logo-input" src="./assets/img/cadena.svg">
-                            <input type="password" name="password" class="style-input" id="password" placeholder="Entrer votre mot de passe" required />
+                            <input type="password" name="password" class="style-input" id="password"
+                                placeholder="Entrer votre mot de passe" required />
                             <button type="button" id="toggle-password" onclick="togglePasswordVisibility()">
-                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" id="eye-open">
-                                    <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z" />
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"
+                                    id="eye-open">
+                                    <path
+                                        d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z" />
                                 </svg>
-                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" id="eye-closed" viewBox="0 0 640 512">
-                                    <path d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L525.6 386.7c39.6-40.6 66.4-86.1 79.9-118.4c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C465.5 68.8 400.8 32 320 32c-68.2 0-125 26.3-169.3 60.8L38.8 5.1zM223.1 149.5C248.6 126.2 282.7 112 320 112c79.5 0 144 64.5 144 144c0 24.9-6.3 48.3-17.4 68.7L408 294.5c8.4-19.3 10.6-41.4 4.8-63.3c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3c0 10.2-2.4 19.8-6.6 28.3l-90.3-70.8zM373 389.9c-16.4 6.5-34.3 10.1-53 10.1c-79.5 0-144-64.5-144-144c0-6.9 .5-13.6 1.4-20.2L83.1 161.5C60.3 191.2 44 220.8 34.5 243.7c-3.3 7.9-3.3 16.7 0 24.6c14.9 35.7 46.2 87.7 93 131.1C174.5 443.2 239.2 480 320 480c47.8 0 89.9-12.9 126.2-32.5L373 389.9z" />
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" id="eye-closed"
+                                    viewBox="0 0 640 512">
+                                    <path
+                                        d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L525.6 386.7c39.6-40.6 66.4-86.1 79.9-118.4c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C465.5 68.8 400.8 32 320 32c-68.2 0-125 26.3-169.3 60.8L38.8 5.1zM223.1 149.5C248.6 126.2 282.7 112 320 112c79.5 0 144 64.5 144 144c0 24.9-6.3 48.3-17.4 68.7L408 294.5c8.4-19.3 10.6-41.4 4.8-63.3c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3c0 10.2-2.4 19.8-6.6 28.3l-90.3-70.8zM373 389.9c-16.4 6.5-34.3 10.1-53 10.1c-79.5 0-144-64.5-144-144c0-6.9 .5-13.6 1.4-20.2L83.1 161.5C60.3 191.2 44 220.8 34.5 243.7c-3.3 7.9-3.3 16.7 0 24.6c14.9 35.7 46.2 87.7 93 131.1C174.5 443.2 239.2 480 320 480c47.8 0 89.9-12.9 126.2-32.5L373 389.9z" />
                                 </svg>
                             </button>
-                            <?php if (isset($error_message)) : ?>
-                                <p class="error-message"><?php echo $error_message; ?></p>
+                            <?php if (isset($error_message)): ?>
+                                <p class="error-message">
+                                    <?php echo $error_message; ?>
+                                </p>
                             <?php endif; ?>
                         </div>
                         <div class="flex-space-between">
