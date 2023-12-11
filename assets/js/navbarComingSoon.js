2dc7fb249bad2e5aca0d0abe05ffbd219e11ac10
Helios Martin <heliosmartin123@gmail.com>
2023-12-11 13:46:06 +0100

    update navbar js

--- assets/js/navbarComingSoon.js
+++ assets/js/navbarComingSoon.js
@@ -1,20 +1,10 @@
 const planningButtons = document.querySelectorAll("#planning-button")
-const playButtons = document.querySelectorAll("#play_button")
 
 planningButtons.forEach((planningButton) => {
     planningButton.addEventListener("click", () => {
         planningButton.innerHTML = "Bientôt..."
         setTimeout(() => {
             planningButton.innerHTML = "Planning"
-        }, 2000)
-    })
-})
-
-playButtons.forEach((playButtons) => {
-    playButtons.addEventListener("click", () => {
-        playButtons.innerHTML = "Bientôt..."
-        setTimeout(() => {
-            playButtons.innerHTML = "Jouer à Shard'Venture"
         }, 2000)
     })
 })
