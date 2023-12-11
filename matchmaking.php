f09b315eb6e0a5e143e5e28a90106c70923a3d6a
Alexandre <alexandre.testa.at@gmail.com>
2023-12-11 14:12:40 +0100

    Création branche Matchmaking et tableau desktop

---
+++ matchmaking.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" href="./assets/img/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="./assets/style/matchmaking.css">
+    <link rel="stylesheet" href="./assets/style/footer.css">
+    <link rel="stylesheet" href="./assets/style/header.css">
+    <title>Matchmaking</title>
+</head>
+
+<body>
+    <header>
+        <?php include('navbar.php'); ?>
+    </header>
+
+    <main>
+        <h1>MATCHMAKING</h1>
+
+        <section>
+            <div class=" equipe equipe1">
+                <div class="rectangle"></div>
+                <div class="nom">
+                    <p>Nom équipe</p>
+                </div>
+            </div>
+            <div class=" equipe equipe2">
+                <div class="rectangle"></div>
+                <div class="nom">
+                    <p>Nom équipe</p>
+                </div>
+            </div>
+            <div class=" equipe equipe3">
+                <div class="rectangle"></div>
+                <div class="nom">
+                    <p>Nom équipe</p>
+                </div>
+            </div>
+            <div class=" equipe equipe4">
+                <div class="rectangle"></div>
+                <div class="nom">
+                    <p>Nom équipe</p>
+                </div>
+            </div>
+            <div class=" equipe equipe5">
+                <div class="rectangle"></div>
+                <div class="nom">
+                    <p>Nom équipe</p>
+                </div>
+            </div>
+            <div class=" equipe equipe6">
+                <div class="rectangle"></div>
+                <div class="nom">
+                    <p>Nom équipe</p>
+                </div>
+            </div>
+            <div class=" equipe equipe7">
+                <div class="rectangle"></div>
+                <div class="nom">
+                    <p>Nom équipe</p>
+                </div>
+            </div>
+            <div class=" equipe equipe8">
+                <div class="rectangle"></div>
+                <div class="nom">
+                    <p>Nom équipe</p>
+                </div>
+            </div>
+            <div class=" equipe equipe9">
+                <div class="rectangle"></div>
+                <div class="nom">
+                    <p>Nom équipe</p>
+                </div>
+            </div>
+            <div class=" equipe equipe10">
+                <div class="rectangle"></div>
+                <div class="nom">
+                    <p>Nom équipe</p>
+                </div>
+            </div>
+            <div class=" equipe equipe11">
+                <div class="rectangle"></div>
+                <div class="nom">
+                    <p>Nom équipe</p>
+                </div>
+            </div>
+            <div class=" equipe equipe12">
+                <div class="rectangle"></div>
+                <div class="nom">
+                    <p>Nom équipe</p>
+                </div>
+            </div>
+            <div class=" equipe equipe13">
+                <div class="rectangle"></div>
+                <div class="nom">
+                    <p>Nom équipe</p>
+                </div>
+            </div>
+            <div class=" equipe equipe14">
+                <div class="rectangle"></div>
+                <div class="nom">
+                    <p>Nom équipe</p>
+                </div>
+            </div>
+
+            <div class="lien lien1"></div>
+            <div class="lien lien2"></div>
+            <div class="lien lien3"></div>
+            <div class="lien lien4"></div>
+            <div class="lien lien5"></div>
+            <div class="lien lien6"></div>
+            <div class="lien lien7"></div>
+            <div class="lien lien8"></div>
+            <div class="lien lien9"></div>
+            <div class="lien lien10"></div>
+            <div class="lien lien11"></div>
+        </section>
+    </main>
+
+    <?php include('footer.php'); ?>
+</body>
+
+</html>
