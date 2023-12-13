eb228a91264622b6354bf1765f257fc7cf933156
Elyesse Kourdourli <elyesse@bigsool.com>
2023-12-13 22:09:33 +0100

    Add TeamScore column for team rankings

--- classement.php
+++ classement.php
@@ -1,10 +1,11 @@
 <?php
+include './connexionbdd.php';
 session_start();
-// Requête SQL pour récupérer le classement des équipes (à adapter selon votre structure de base de données)
+
 $sql = "SELECT * FROM team ORDER BY TeamScore DESC";
-
-$result = $conn->query($sql);
-
+$stmt = $db->prepare($sql);
+$stmt->execute();
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
@@ -21,48 +22,36 @@ $result = $conn->query($sql);
     <link rel="stylesheet" href="./assets/style/classement.css">
 </head>
 
-
-
 <body>
     <?php include './navbar.php'; ?>
     <h1 class="en-tete">LES ÉQUIPES DE LA MMI LAN</h1>
     <div id="board-container">
-        <div id="board-title" class="board-title">l’équipe vainqueur est: </div>
+        <div id="board-title" class="board-title">L’équipe vainqueur est: </div>
         <div id="board" class="leaderboard">
             <?php
-            // Afficher les données du classement
-            if ($result->num_rows > 0) {
+            if (count($result) > 0) {
                 $position = 1;
-            while ($row = $result->fetch_assoc()) {
-                $class = ($position == 1) ? 'first-place' : (($position == 2) ? 'second-place' : (($position == 3) ? 'third-place' : 'normal-place'));
+                foreach ($result as $row) {
+                    $class = ($position == 1) ? 'first-place' : (($position == 2) ? 'second-place' : (($position == 3) ? 'third-place' : 'normal-place'));
 
-            if ($position > 3) {
-                echo "<div class='team-block'>";
+                    echo "<div class='team-block " . ($position > 3 ? "" : $class) . "'>";
+                    echo "<div class='team-info'>";
+                    echo "<img src='" . htmlspecialchars($row["TeamLogo"]) . "' alt='Team Logo' class='team-logo'>";
+                    echo "<div class='team-details'>";
+                    echo "<h3>" . htmlspecialchars($row["TeamName"]) . "</h3>";
+                    echo "<p>" . htmlspecialchars($row["TeamScore"]) . " Points</p>";
+                    echo "</div>"; 
+                    echo "</div>"; 
+                    echo "</div>"; 
+                    $position++;
+                }
             } else {
-                echo "<div class='team-block $class'>";
-            }
-
-            echo "<div class='team-info'>";
-            echo "<img src='" . $row["TeamLogo"] . "' alt='Team Logo' class='team-logo'>";
-            echo "<div class='team-details'>";
-            echo "<h3>" . $row["TeamName"] . "</h3>";
-            echo "<p>" . $row["TeamScore"] . " Points</p>";
-            echo "</div>"; // Fermeture de la div team-details
-            echo "</div>"; // Fermeture de la div team-info
-            echo "</div>"; // Fermeture de la div team-block
-            $position++;
-            }
-            } else {
-            echo "<p>Aucune équipe trouvée.</p>";
+                echo "<p>Aucune équipe trouvée.</p>";
             }
             ?>
-
         </div>
     </div>
     <?php include './footer.php'; ?>
 </body>
 
 </html>
-<?php
-$conn->close();
-?>
