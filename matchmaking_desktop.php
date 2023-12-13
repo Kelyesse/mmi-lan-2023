84df1437146f9dcd81e97ca793de9d64d666f890
Alexandre <alexandre.testa.at@gmail.com>
2023-12-13 16:43:43 +0100

    rajout link css matchmaking desktop

--- matchmaking_desktop.php
+++ matchmaking_desktop.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" href="./assets/img/favicon.png" type="image/x-icon">
-    <link rel="stylesheet" href="./assets/style/matchmaking.css">
+    <link rel="stylesheet" href="./assets/style/matchmaking_desktop.css">
     <link rel="stylesheet" href="./assets/style/footer.css">
     <link rel="stylesheet" href="./assets/style/header.css">
     <title>Matchmaking</title>
